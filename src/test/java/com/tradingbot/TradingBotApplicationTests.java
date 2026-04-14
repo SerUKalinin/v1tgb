c116adf593ccdba52ee7dddd415518cc2ffa13f3
Сергей Калинин <serukalinin@gmail.com>
2026-04-14 15:29:33 +0300

    dev

--- src/test/java/com/tradingbot/TradingBotApplicationTests.java
+++ src/test/java/com/tradingbot/TradingBotApplicationTests.java
@@ -2,8 +2,11 @@ package com.tradingbot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = "spring.flyway.enabled=false")
+@ActiveProfiles("test")
 class TradingBotApplicationTests {
 
 	@Test
