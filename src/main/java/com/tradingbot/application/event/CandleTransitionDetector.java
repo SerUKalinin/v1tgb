22e7e6717ebc1f19c8ce38e61a2c91d14dc9eca9
Сергей Калинин <serukalinin@gmail.com>
2026-04-24 23:01:53 +0300

    dev

--- src/main/java/com/tradingbot/application/event/CandleTransitionDetector.java
+++ src/main/java/com/tradingbot/application/event/CandleTransitionDetector.java
@@ -46,6 +46,8 @@ public class CandleTransitionDetector {
             if (current == null || !current.equals(openTime)) {
                 if (processedCandleRepository.markAsProcessed(symbol, openTime)) {
                     isNew.set(true);
+
+
                     return openTime;
                 }
             }
